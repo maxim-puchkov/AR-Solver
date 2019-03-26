c907742543df8f812771f2437c4ccb2cb0523c82
mpuchkov <mpuchkov@sfu.ca>
2019-03-26 09:37:22 -0700

    Empty Xcode project created

---
+++ MathVision/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MathVision: MathVision
+//  com.maximpuchkov.Untitled 1.MathVision.ViewController
+//
+//  Created by mpuchkov on 2019-03-26. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MathVisionTests/MathVisionTests.swift
@@ -0,0 +1,35 @@
+//
+//  MathVisionTests.swift
+//  MathVision: MathVisionTests
+//  com.maximpuchkov.Untitled 1.MathVision.MathVisionTests
+//
+//  Created by mpuchkov on 2019-03-26. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+import XCTest
+@testable import MathVision
+
+class MathVisionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
