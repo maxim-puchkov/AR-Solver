51ad29915c67eaae510902b2753742de635fce80
mpuchkov <mpuchkov@sfu.ca>
2019-03-26 19:00:06 -0700

    OCR: ViewController initial tests removed

--- MathVision/ViewController.swift
+++ MathVision/ViewController.swift
@@ -8,43 +8,18 @@
 //
 
 import UIKit
-import TesseractOCR
 
-class ViewController: UIViewController, G8TesseractDelegate {
-    
-    
-    let tess: G8Tesseract = G8Tesseract(language: "eng")
-    let image = UIImage(named: "Text1")
-    let alphanum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-    
+class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // Test OCR
-        print("Testing optical character recognition...")
-        
-        tess.delegate = self
-        
-        tess.charWhitelist = alphanum
-        
-        tess.image = image
-        
-        tess.recognize()
-        
-        let imageText = tess.recognizedText!
-        
-        print("Output: \(imageText)")
-        // Test ends
-        
     }
     
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
     
 }
 
