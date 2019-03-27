5129a1cc2a26f2b23dcb4ab239d74afb42ff0c8e
mpuchkov <mpuchkov@sfu.ca>
2019-03-26 18:43:18 -0700

    Recognizer.swift file added

---
+++ MathVision/Recognizer.swift
@@ -0,0 +1,10 @@
+//
+//  Recognizer.swift
+//  MathVision: MathVision
+//  com.maximpuchkov.MathVision.MathVision.Recognizer
+//
+//  Created by mpuchkov on 2019-03-26. macOS 10.13, Xcode 10.1.
+//  Copyright © 2019 Maxim Puchkov. All rights reserved.
+//
+
+import Foundation
