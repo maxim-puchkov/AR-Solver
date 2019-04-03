e289fd62f72456fbd0b9b15fccb780453fc006ba
Austin Whitney <awhitney@asb9700-e03.csil.sfu.ca>
2019-04-03 15:23:29 -0700

    Fixed Solver Function

--- MathVision/stringSolver.swift
+++ MathVision/stringSolver.swift
@@ -5,24 +5,31 @@
 //  Created by Austin Whitney on 2019-03-29.
 //  Copyright © 2019 maximpuchkov. All rights reserved.
 //
-
 import Foundation
-
-import Foundation
-
 func solve(formula: String, input: Float) -> NSNumber {
     let letterset = CharacterSet.letters
     let numberset = CharacterSet.decimalDigits
     var newformula = formula
     for index in newformula.indices{
+        print(newformula)
         if letterset.contains(newformula.unicodeScalars[index]) {
             if(index > newformula.startIndex){
                 if numberset.contains(newformula.unicodeScalars[newformula.index(before: index)]) {
-                    newformula = newformula.replacingOccurrences(of: String(newformula[index]), with: String(input))
+                    newformula.remove(at: index)
+                    newformula.insert(contentsOf: String(input), at: index)
                     newformula.insert("*", at: index)
+                    return solve(formula: newformula, input: input)
                 }
                 else {
-                    newformula = newformula.replacingOccurrences(of: String(newformula[index]), with: String(input))                }
+                    newformula.remove(at: index)
+                    newformula.insert(contentsOf: String(input), at: index)
+                    return solve(formula: newformula, input: input)
+                }
+            }
+            else{
+                newformula.remove(at: index)
+                newformula.insert(contentsOf: String(input), at: index)
+                return solve(formula: newformula, input: input)
             }
             
         }
